82969e7ff80bfc65ce1c80a32b8999224333e2fb
Alisha Kodibagkar <alishakodibagkar@Alishas-MacBook-Pro.local>
2023-01-17 12:51:32 -0500

    add activities to master

---
+++ ui-changes/src/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  /* eslint-disable */
+  githubSrc: 'https://raw.githubusercontent.com/alishakodibagkar/MumbleMelody_Longitudinal_Screening/master/protocol/Screening_schema',
+  startButton: {
+    "en": 'Join'
+  },
+  banner: 'Eligibility Screening for Mumble Melody Longitudinal Study',
+  assetsPublicPath: '/MumbleMelody_Longitudinal_Screening/',
+  contact: 'akodiba1@mit.edu',
+  showHelp: true,
+  checkMediaPermission: true
+};
+
